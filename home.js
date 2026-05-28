ad69c1cacef31e23ad4d6b5f3452adb9a31568cd
fayzullohabdusoliqov-hue <fayzulloh>
2026-05-28 16:12:12 +0500

    start skill rush

--- home.js
+++ home.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBOZxD61FFMXBscDzQihhxexMvD-cHhabw",
   authDomain: "skillrush-3adaf.firebaseapp.com",
@@ -16,7 +11,6 @@ const firebaseConfig = {
   measurementId: "G-WKHYQ00ML5"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
