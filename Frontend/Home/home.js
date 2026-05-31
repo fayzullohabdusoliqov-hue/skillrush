3156ac62913ec346b5437532b7a3de92f61b2316
fayzullohabdusoliqov-hue <fayzullohabdusoliqov@gmail.com>
2026-05-31 16:20:56 +0500

    skill rush home finaly

--- Frontend/Home/home.js
+++ Frontend/Home/home.js
@@ -6,19 +6,91 @@ const headerBall = document.querySelector(".ball")
 const localId = localStorage.getItem("localId")
 const token = localStorage.getItem("token")
 
+
+const profileForm = document.querySelector(".profile__form")
+const profileInput = document.querySelector("#name")
+const profileLabel = document.querySelector(".profile_label")
+const skinsList = document.querySelector(".skins__list")
+
 async function getProfile(){
     try{
         const res = await fetch(`https://skillrush-3adaf-default-rtdb.firebaseio.com/profile/${localId}.json?auth=${token}`)
         const data = await res.json()
-        console.log(data)
 
         headerName.textContent = `${data?.name}`
         headerEmail.textContent = `${data?.email}`
         headerAvatar.src = `${data?.avatar}`
         headerScore.textContent = `${data?.score}`
         headerBall.textContent = `${data?.ball}`
+        renderSkins(data?.skins)
     }catch(err){
         console.log(err.message)
     }
 }
 getProfile()
+
+profileInput.addEventListener("input", () => {
+    let nameLength = profileInput.value
+    profileLabel.textContent = `21/${nameLength.length}`
+
+    if(nameLength.length > 21){
+        profileLabel.style.color = "red"
+        profileLabel.textContent = "Your name have to 21 worbs"
+    }else{
+        profileLabel.style.color = "#dfdffdcb"
+        profileLabel.textContent = `21/${nameLength.length}`
+    }
+})
+
+async function patchName(){
+    try{
+        const res = await fetch(`https://skillrush-3adaf-default-rtdb.firebaseio.com/profile/${localId}.json?auth=${token}`,{
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`
+            },
+            body:JSON.stringify({
+                name: profileInput.value
+            })
+        })
+        const data = await res.json()
+        console.log(data)
+    }catch(err){
+        console.log("hatolik" + err.message)
+    }finally{
+        window.location.reload()
+    }
+}
+profileForm.addEventListener("submit", (evt) => {
+    evt.preventDefault()
+    patchName()
+})
+
+function renderSkins(skins){
+    for(let i = 0; i < skins.length; i++){
+        skinsList.innerHTML += `<li data-url="${skins[i].img}" class="skins_item"><img width="90" src=${skins[i].img} alt="" class="skins_img"></li>`
+    }
+}
+skinsList.addEventListener("click", async(evt) => {
+    const clickItem = evt.target.closest(".skins_item")
+    const clickItemUrl = clickItem.dataset.url
+    
+    try{
+        const res = await fetch(`https://skillrush-3adaf-default-rtdb.firebaseio.com/profile/${localId}.json?auth=${token}`,{
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                avatar: clickItemUrl
+            })
+        })
+        const data = await res.json
+    }catch(err){
+        console.log(err.message)
+    }finally{
+        window.location.reload()
+    }
+})
---
+++ Frontend/Leader/leader.js
@@ -0,0 +1,1 @@
+console.leg("hallo world")
