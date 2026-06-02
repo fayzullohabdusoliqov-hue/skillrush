01d04592726b0ae141e0a1be4c77536efc1f00d7
fayzullohabdusoliqov-hue <fayzulloh>
2026-06-02 14:24:31 +0500

    skill rush

--- Frontend/Leader/leader.js
+++ Frontend/Leader/leader.js
@@ -6,6 +6,14 @@ const headerBall = document.querySelector(".ball")
 const localId = localStorage.getItem("localId")
 const token = localStorage.getItem("token")
 
+const leaderList = document.querySelector(".leader__list")
+const leaderTopOneName = document.querySelector("#top_one-name")
+const leaderTopTwoName = document.querySelector("#top_two-name")
+const leaderTopThreeName = document.querySelector("#top_three-name")
+const leaderTopOneAvatar = document.querySelector("#top_one-avatar")
+const leaderTopTwoAvatar = document.querySelector("#top_two-avatar")
+const leaderTopThreeAvatar = document.querySelector("#top_three-avatar")
+
 async function getProfile(){
     try{
         const res = await fetch(`https://skillrush-3adaf-default-rtdb.firebaseio.com/profile/${localId}.json?auth=${token}`)
@@ -20,4 +28,40 @@ async function getProfile(){
         console.log(err.message)
     }
 }
+async function getLeaders(){
+    try{
+        const res = await fetch("https://skillrush-3adaf-default-rtdb.firebaseio.com/profile.json")
+        const resData = await res.json()
+        const arrayData = await Object.values(resData)
+        const sortData = await [...arrayData].sort((a,b) => b.ball - a.ball)
+
+        leaderTopOneAvatar.src = `${sortData[0].avatar}`
+        leaderTopOneName.textContent = `${sortData[0].name}`
+        leaderTopTwoAvatar.src = `${sortData[1].avatar}`
+        leaderTopTwoName.textContent = `${sortData[1].name}`
+        leaderTopThreeAvatar.src = `${sortData[2].avatar}`
+        leaderTopThreeName.textContent = `${sortData[2].name}`
+
+        renderLeader(sortData)
+    }catch(err){
+        console.log(err.message)
+    }
+}
+function renderLeader(sortData){
+    leaderList.innerHTML = ""
+    for(let i = 0; i < sortData.length; i++){
+        leaderList.innerHTML += `<li class="leader_item">
+            <div class="leader__left">
+                <span class="leader_reating">${i + 1}</span>
+                <img src="${sortData[i].avatar}" alt="" class="leader_avatar">
+                <h3 class="leader_subtitle">${sortData[i].name}</h3>
+            </div>
+            <div class="leader__right">
+                <strong class="leader_ball">${sortData[i].ball} ball</strong>
+                <strong class="leader_score">${sortData[i].score} score</strong>
+            </div>
+        </li>`
+    }
+}
+getLeaders()
 getProfile()
