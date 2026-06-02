733719a69b8c6709e11700734adcfc8ad810047b
fayzullohabdusoliqov-hue <fayzulloh>
2026-06-02 11:27:01 +0500

    skill rush

--- Frontend/Leader/leader.js
+++ Frontend/Leader/leader.js
@@ -1,1 +1,25 @@
-console.leg("hallo world")
+const headerName = document.querySelector(".header_name")
+const headerEmail = document.querySelector(".header_email")
+const headerAvatar = document.querySelector(".header_avatar")
+const headerScore = document.querySelector(".score")
+const headerBall = document.querySelector(".ball")
+const localId = localStorage.getItem("localId")
+const token = localStorage.getItem("token")
+
+async function getProfile(){
+    try{
+        const res = await fetch(`https://skillrush-3adaf-default-rtdb.firebaseio.com/profile/${localId}.json?auth=${token}`)
+        const data = await res.json()
+
+        headerName.textContent = `${data?.name}`
+        headerEmail.textContent = `${data?.email}`
+        headerAvatar.src = `${data?.avatar}`
+        headerScore.textContent = `${data?.score}`
+        headerBall.textContent = `${data?.ball}`
+        const filterNull = data?.skins?.filter((el) => el != null)
+        renderSkins(filterNull)
+    }catch(err){
+        console.log(err.message)
+    }
+}
+getProfile()
